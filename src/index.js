a2b2f6a9e51ab7d271f3047564da58e7e49913ed
K1ndum <kindum98@gmail.com>
2021-07-11 10:11:17 +0300

    finish task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined || array.length == 0) {
+    return 0;
+  } else {
+    let numMin = array[0];
+    for (let i = 1; i <= array.length; i++) {
+      if (numMin > array[i]) {
+      numMin = array[i];
+      }
+  }
+    return numMin;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined || array.length == 0) {
+    return 0;
+  } else {
+    let numMax = array[0];
+    for (let i = 1; i <= array.length; i++) {
+      if (numMax < array[i]) {
+      numMax = array[i];
+      } 
+    }
+      return numMax;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined || array.length == 0) {
+    return 0;
+  } else {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+  sum += array[i];
+  }
+  return sum/array.length;
+  }
 }
